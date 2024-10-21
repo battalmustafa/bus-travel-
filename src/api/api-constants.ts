b19bcb767db5b440b7aa922a3a70766d27d87ae9
m.battal <m.battal@atrtouristik.com>
2024-10-22 02:11:21 +0300

    fix:redirects

--- src/api/api-constants.ts
+++ src/api/api-constants.ts
@@ -7,6 +7,4 @@ export const ENDPOINTS = {
   GET_SESSION: `/api/client/getsession`,
   GET_BUS_LOCATIONS: `/api/location/getbuslocations`,
   GET_BUS_JOURNEYS: `/api/journey/getbusjourneys`,
-  GET_BUS_JOURNEY: `api/journey/getbusjourneydetail`,
-  // Add other endpoints here...
 } as const;
